26a1fdc9a9ffb35a92dfbe1779122375dd9c85b1
Petur Olsen <github@pdur.dk>
2017-05-29 10:15:23 +0200

    added JSON support

--- aggregator-manager/src/main/java/org/arrowhead/wp5/aggmanager/impl/HTTPClientTest.java
+++ aggregator-manager/src/main/java/org/arrowhead/wp5/aggmanager/impl/HTTPClientTest.java
@@ -26,6 +26,7 @@ package org.arrowhead.wp5.aggmanager.impl;
  * #L%
  */
 
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -56,7 +57,7 @@ public class HTTPClientTest {
         List<FlexOfferSlice> slices = new ArrayList<FlexOfferSlice>();
         int loadLow[] = new int[] { -500, 1700, -1050, 1100, 500, 1400, 400 };
         int loadHigh[] = new int[] { 500, 1732, -500, 1168, 552, 1471, 453 };
-        int sliceDuration = 4;
+        int sliceDuration = 1;
 
         for (int i = 0; i < loadLow.length; i++) {
             slices.add(new FlexOfferSlice(sliceDuration, 1, loadLow[i], loadHigh[i]));
@@ -90,21 +91,35 @@ public class HTTPClientTest {
         flexOffer.setStartBeforeTime(cal.getTime());
         flexOffer.setOfferedById(id);
         flexOffer.setId(1);
+        flexOffer.setDefaultSchedule(new FlexOfferSchedule(flexOffer));
 
         Client client = ClientBuilder.newClient();
-        
+
+        boolean json = false;
+
+        String mtype;
+        Entity<FlexOffer> ent;
+        if (json) {
+            mtype = MediaType.APPLICATION_JSON;
+            ent = Entity.json(flexOffer);
+        } else {
+            mtype = MediaType.APPLICATION_XML;
+            ent = Entity.xml(flexOffer);
+        }
+
         WebTarget target = client.target(BASE_URI).path("api").path("flexoffers").path(id);
         System.out.println(target.getUri());
-        Response resp = target.request().accept(MediaType.APPLICATION_XML).post(Entity.xml(flexOffer));
+        Response resp = target.request().accept(mtype).post(ent);
         System.out.println(resp.getStatus());
         System.out.println(resp.getStatusInfo());
+        System.out.println(resp.readEntity(String.class));
 
 
         if (resp.getStatus() == 200) {
             FlexOfferSchedule fos = null;
             while ((fos = flexOffer.getFlexOfferSchedule()) == null) {
                 target = client.target(BASE_URI).path("api").path("flexoffers").path(id).path(Integer.toString(flexOffer.getId()));
-                resp = target.request().get();
+                resp = target.request().accept(mtype).get();
                 System.out.println(resp.getStatus());
                 flexOffer = resp.readEntity(FlexOffer.class);
                 try {
